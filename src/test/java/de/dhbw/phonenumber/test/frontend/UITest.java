5e41d1ff4bbdcf724da0413ca7b97f463b25ef86
Nico Dreher <Nico.Dreher98@gmail.com>
2020-05-06 13:02:24 +0200

    Added CI

---
+++ src/test/java/de/dhbw/phonenumber/test/frontend/UITest.java
@@ -0,0 +1,12 @@
+package de.dhbw.phonenumber.test.frontend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UITest {
+    @Test
+    public void test() {
+        System.out.println("!!!!Test");
+    }
+}
